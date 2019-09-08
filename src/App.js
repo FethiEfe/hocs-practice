c21cef95a5950950d935d38334a9266714324646
FETHI AKCAY <f.akcay1@gmail.com>
2019-09-08 18:19:50 -0500

    simple practice of Hocs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import ClickCounter from "./components/ClickCounter"
+import HoverCounter from "./components/HoverCounter"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClickCounter name = "Fethi"/>
+      <HoverCounter name = "You know who"/>
     </div>
   );
 }
